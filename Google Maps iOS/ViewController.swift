aaa7244ceb0870949e3edd52568ecc51ec1a2261
Sri Chandra Mallipeddi <hr.srichandra@gmail.com>
2016-01-01 17:48:17 -0800

    Google Maps for iOS with Swift
    Displaying a Map.
    Changing Map type.
    User’s location on the map.
    Spotting a custom Location.
    Adding a Marker.
    Drawing a route.
    Adding intermediate locations/waypoints to a route. Etc.

---
+++ Google Maps iOS/ViewController.swift
@@ -0,0 +1,265 @@
+//
+//  ViewController.swift
+//  Google Maps iOS
+//
+//  Created by Sri Chandra Mallipeddi on 12/31/15.
+//  Copyright © 2015 Sri Chandra Mallipeddi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController, CLLocationManagerDelegate {
+    
+    @IBOutlet weak var viewMap: GMSMapView!
+    
+    @IBOutlet weak var bbFindAddress: UIBarButtonItem!
+    
+    @IBOutlet weak var lblInfo: UILabel!
+    
+    var locationManager = CLLocationManager()
+    
+    var didFindMyLocation = false
+    
+    var mapTasks = MapTasks()
+    
+    var locationMarker: GMSMarker!
+    
+    var originMarker: GMSMarker!
+    
+    var destinationMarker: GMSMarker!
+    
+    var routePolyline: GMSPolyline!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        //let camera: GMSCameraPosition = GMSCameraPosition.cameraWithLatitude(36.155091, longitude: -123.328653, zoom: 5.0)
+       //viewMap.camera = camera
+        
+        locationManager.delegate = self
+        locationManager.requestWhenInUseAuthorization()
+        viewMap.addObserver(self, forKeyPath: "myLocation", options: NSKeyValueObservingOptions.New, context: nil)
+        
+        
+        
+        /*
+        let camera = GMSCameraPosition.cameraWithLatitude(-33.86,
+            longitude: 151.20, zoom: 6)
+        let mapView = GMSMapView.mapWithFrame(CGRectZero, camera: camera)
+        mapView.myLocationEnabled = true
+        self.view = mapView
+        
+        let marker = GMSMarker()
+        marker.position = CLLocationCoordinate2DMake(-33.86, 151.20)
+        marker.title = "Sydney"
+        marker.snippet = "Australia"
+        marker.map = mapView
+        */
+        
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    
+    override func observeValueForKeyPath(keyPath: String?, ofObject object: AnyObject?, change: [String : AnyObject]?, context: UnsafeMutablePointer<Void>) {
+    //override func observeValueForKeyPath(keyPath: String, ofObject object: AnyObject, change: [NSObject : AnyObject], context: UnsafeMutablePointer<Void>) {
+        if !didFindMyLocation {
+            let myLocation: CLLocation = change?[NSKeyValueChangeNewKey] as! CLLocation
+            viewMap.camera = GMSCameraPosition.cameraWithTarget(myLocation.coordinate, zoom: 10.0)
+            viewMap.settings.myLocationButton = true
+            
+            didFindMyLocation = true
+        }
+    }
+
+    
+    // MARK: IBAction method implementation
+    
+    @IBAction func changeMapType(sender: AnyObject) {
+        let actionSheet = UIAlertController(title: "Map Types", message: "Select map type:", preferredStyle: UIAlertControllerStyle.ActionSheet)
+        
+        let normalMapTypeAction = UIAlertAction(title: "Normal", style: UIAlertActionStyle.Default) { (alertAction) -> Void in
+            self.viewMap.mapType = kGMSTypeNormal
+        }
+        
+        let terrainMapTypeAction = UIAlertAction(title: "Terrain", style: UIAlertActionStyle.Default) { (alertAction) -> Void in
+            self.viewMap.mapType = kGMSTypeTerrain
+        }
+        
+        let hybridMapTypeAction = UIAlertAction(title: "Hybrid", style: UIAlertActionStyle.Default) { (alertAction) -> Void in
+            self.viewMap.mapType = kGMSTypeHybrid
+        }
+        
+        let cancelAction = UIAlertAction(title: "Close", style: UIAlertActionStyle.Cancel) { (alertAction) -> Void in
+            
+        }
+        
+        actionSheet.addAction(normalMapTypeAction)
+        actionSheet.addAction(terrainMapTypeAction)
+        actionSheet.addAction(hybridMapTypeAction)
+        actionSheet.addAction(cancelAction)
+        
+        presentViewController(actionSheet, animated: true, completion: nil)
+        
+    }
+    
+    
+    @IBAction func findAddress(sender: AnyObject) {
+        let addressAlert = UIAlertController(title: "Address Finder", message: "Type the address you want to find:", preferredStyle: UIAlertControllerStyle.Alert)
+        
+        addressAlert.addTextFieldWithConfigurationHandler { (textField) -> Void in
+            textField.placeholder = "Address?"
+        }
+        
+        let findAction = UIAlertAction(title: "Find Address", style: UIAlertActionStyle.Default) { (alertAction) -> Void in
+            let address = (addressAlert.textFields![0] as UITextField).text! as String
+            
+            self.mapTasks.geocodeAddress(address, withCompletionHandler: { (status, success) -> Void in
+                    if !success {
+                        print(status)
+                        
+                        if status == "ZERO_RESULTS" {
+                            self.showAlertWithMessage("The location could not be found.")
+                        }
+                    }
+                    else {
+                        let coordinate = CLLocationCoordinate2D(latitude: self.mapTasks.fetchedAddressLatitude, longitude: self.mapTasks.fetchedAddressLongitude)
+                        self.viewMap.camera = GMSCameraPosition.cameraWithTarget(coordinate, zoom: 14.0)
+                        self.setuplocationMarker(coordinate)
+                    }
+                
+                
+            })
+            
+        }
+        
+        let closeAction = UIAlertAction(title: "Close", style: UIAlertActionStyle.Cancel) { (alertAction) -> Void in
+            
+        }
+        
+        addressAlert.addAction(findAction)
+        addressAlert.addAction(closeAction)
+        
+        presentViewController(addressAlert, animated: true, completion: nil)
+        
+    }
+    
+    
+    @IBAction func createRoute(sender: AnyObject) {
+        let addressAlert = UIAlertController(title: "Create Route", message: "Connect locations with a route:", preferredStyle: UIAlertControllerStyle.Alert)
+        
+        addressAlert.addTextFieldWithConfigurationHandler { (textField) -> Void in
+            textField.placeholder = "Origin?"
+        }
+        
+        addressAlert.addTextFieldWithConfigurationHandler { (textField) -> Void in
+            textField.placeholder = "Destination?"
+        }
+        
+        
+        let createRouteAction = UIAlertAction(title: "Create Route", style: UIAlertActionStyle.Default) { (alertAction) -> Void in
+            let origin = (addressAlert.textFields![0] as UITextField).text! as String
+            let destination = (addressAlert.textFields![1] as UITextField).text! as String
+            
+            self.mapTasks.getDirections(origin, destination: destination, waypoints: nil, travelMode: nil, completionHandler: { (status, success) -> Void in
+                if success {
+                    self.configureMapAndMarkersForRoute()
+                    self.drawRoute()
+                    self.displayRouteInfo()
+                }
+                else {
+                    print(status)
+                }
+            })
+        }
+        
+        let closeAction = UIAlertAction(title: "Close", style: UIAlertActionStyle.Cancel) { (alertAction) -> Void in
+            
+        }
+        
+        addressAlert.addAction(createRouteAction)
+        addressAlert.addAction(closeAction)
+        
+        presentViewController(addressAlert, animated: true, completion: nil)
+    }
+    
+    
+    @IBAction func changeTravelMode(sender: AnyObject) {
+        
+    }
+    
+    
+    // MARK: Custom method implementation
+    
+    func showAlertWithMessage(message: String) {
+        let alertController = UIAlertController(title: "GMapsDemo", message: message, preferredStyle: UIAlertControllerStyle.Alert)
+        
+        let closeAction = UIAlertAction(title: "Close", style: UIAlertActionStyle.Cancel) { (alertAction) -> Void in
+            
+        }
+        
+        alertController.addAction(closeAction)
+        
+        presentViewController(alertController, animated: true, completion: nil)
+    }
+    
+    func setuplocationMarker(coordinate: CLLocationCoordinate2D) {
+        if locationMarker != nil {
+            locationMarker.map = nil
+        }
+        locationMarker = GMSMarker(position: coordinate)
+        locationMarker.map = viewMap
+        
+        locationMarker.title = mapTasks.fetchedFormattedAddress
+        locationMarker.appearAnimation = kGMSMarkerAnimationPop
+        locationMarker.icon = GMSMarker.markerImageWithColor(UIColor.blueColor())
+        locationMarker.opacity = 0.75
+        locationMarker.flat = true
+        locationMarker.snippet = "My home."
+    }
+    
+    
+    func configureMapAndMarkersForRoute() {
+        viewMap.camera = GMSCameraPosition.cameraWithTarget(mapTasks.originCoordinate, zoom: 9.0)
+        
+        
+        originMarker = GMSMarker(position: self.mapTasks.originCoordinate)
+        originMarker.map = self.viewMap
+        originMarker.icon = GMSMarker.markerImageWithColor(UIColor.greenColor())
+        originMarker.title = self.mapTasks.originAddress
+        
+        destinationMarker = GMSMarker(position: self.mapTasks.destinationCoordinate)
+        destinationMarker.map = self.viewMap
+        destinationMarker.icon = GMSMarker.markerImageWithColor(UIColor.redColor())
+        destinationMarker.title = self.mapTasks.destinationAddress
+        originMarker = GMSMarker(position: self.mapTasks.originCoordinate)
+        originMarker.map = self.viewMap
+        originMarker.icon = GMSMarker.markerImageWithColor(UIColor.greenColor())
+        originMarker.title = self.mapTasks.originAddress
+        
+        destinationMarker = GMSMarker(position: self.mapTasks.destinationCoordinate)
+        destinationMarker.map = self.viewMap
+        destinationMarker.icon = GMSMarker.markerImageWithColor(UIColor.redColor())
+        destinationMarker.title = self.mapTasks.destinationAddress    
+    }
+    
+    
+    func drawRoute() {
+        let route = mapTasks.overviewPolyline["points"] as! String
+        
+        let path: GMSPath = GMSPath(fromEncodedPath: route)
+        routePolyline = GMSPolyline(path: path)
+        routePolyline.map = viewMap
+    }
+    
+    func displayRouteInfo() {
+        lblInfo.text = mapTasks.totalDistance + "\n" + mapTasks.totalDuration
+    }
+
+    
+
+}
+
